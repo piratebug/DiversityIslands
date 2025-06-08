ca23271d12bae1bfabb3057254cde04478b1f281
Alex Clasen <lackadaisicalladybug@gmail.com>
2025-06-07 20:20:08 -0700

    Adds arcpy messages, cleans up table data, sets up script for real world inputs

--- DiversityIndexCalculator.py
+++ DiversityIndexCalculator.py
@@ -25,23 +25,24 @@ import pandas as pd
 
 
 # Set workspace as a user specified parameter
-ws = #arcpy.GetParameterAsText(0) # or your filepath here, for testing
+ws = arcpy.GetParameterAsText(0)   # Or your filepath here, for testing
 arcpy.env.workspace = ws
 
 
 # Inputs
-#inputTable = arcpy.GetParameterAsText(1) #table user will be working with
-#totalPopluation = arcpy.GetParameterAsText(2) #user input field
-#totalPopDivList = arcpy.GetParameterAsText(3) #user input multiple columns
-inputTable = 'sample.csv'
-totalPopulation = 'Total'
-totalPopDivList = ['White', 'Black', 'Native', 'Asian', 'Other']
+inputTable = arcpy.GetParameterAsText(1)       # table user will be working with
+totalPopulation = arcpy.GetParameterAsText(2)  # user input field
+totalPopDivList = [arcpy.GetParameterAsText(3)]  # user input multiple columns
+#inputTable = 'sample.csv'
+#totalPopulation = 'Total'
+#totalPopDivList = ['White', 'Black', 'Native', 'Asian', 'Other']
 
 
 # Calculations
 def calculateDivIndex(inputTable, totalPopulation, totalPopDivList):
     data = pd.read_csv(inputTable)
     totalDivDivList = []
+    forRemoval = []
     sumData = 0.0
 
     try:
@@ -51,12 +52,17 @@ def calculateDivIndex(inputTable, totalPopulation, totalPopDivList):
             divDiv = div + " Div"   # diversification (percentages * natural logs)
             # Create columns for and calculate percentages for each diversity column
             data[divPerc] = data[div] / data[totalPopulation]
+            forRemoval.append(data[divPerc])
+            arcpy.AddMessage('Calculating diversity percentages...')
             # Create columns for and calculate natural log of each diversity percentage
             data[divLog] = np.log(data[divPerc])
+            forRemoval.append(data[divLog])
+            arcpy.AddMessage('Calculating natural logs...')
             # Errors with inf/-inf when log encounters '0' in divPerc, replaces infs with 0
             data[divLog].replace([np.inf, -np.inf], 0, inplace = True)
             # Multiply each population percentage by its Natural Log
             data[divDiv] = data[divPerc] * data[divLog]
+            arcpy.AddMessage('Calculating diversification...')
             # Create list of diversifictions to iterate through later
             totalDivDivList.append(data[divDiv])
 
@@ -65,16 +71,29 @@ def calculateDivIndex(inputTable, totalPopulation, totalPopDivList):
             sumData = sumData + div
             # Inverse of the sum becomes the Diversity Index
             data['Diversity Index'] = -sumData
+            arcpy.AddMessage('Calculating diversity index...')
 
-        print(data.head())
-        print(data['Diversity Index'])
-        data.to_csv('output.csv')
+        # Clean up columns
+        for div in totalPopDivList:
+            divPerc = div + " Perc" # population percentages
+            divLog = div + " Log"   # natural logs of percentages
+            divDiv = div + " Div"   # diversification (percentages * natural logs)
+
+            del data[divPerc]
+            del data[divLog]
+            del data[divDiv]
+        arcpy.AddMessage('Cleaning up data...')
+
+
+        data.to_csv(inputTable)
 
     except Exception as issue:
         print("Error occured: ", issue)
         
 
-## Add ArcPy messages
+
+
+
 
 
 if __name__ == "__main__":
