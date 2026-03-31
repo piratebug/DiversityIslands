4f44b1218545832390cb720dde3f75bec4e4a1e3
Alex Clasen <lackadaisicalladybug@gmail.com>
2026-03-30 17:20:31 -0700

    Removing columns

--- PrepCensusData.py
+++ PrepCensusData.py
@@ -4,9 +4,9 @@ Project: Prep Census Data tool to clean up demographic census .csv's before
 running Diversity Index Calculator.
 
 functionality includes...
-- remove first row & last row
-- remove second column (geographic area name)
-- remove all columns that contain "margin of error"
+X remove first row & last row
+X remove second column (geographic area name)
+X remove all columns that contain "margin of error"
 - rename first column header TOTAL_POP
 - remove "Estimate!!Total:!!" from all other column headers
 - calculate FIPS from GeoID (right 11 digits)
@@ -24,7 +24,7 @@ censusData = pd.read_csv("complexSample.csv") #arcpy.GetParameterAsText(0)  #OR 
 # Calculations
 def cleanData(censusData):
     try:
-        print(censusData) # for testing
+        print(censusData[:5]) # for testing
 
         # set headers to the the contents of the first row, then delete first row
         censusData.columns = censusData.iloc[0]
@@ -37,14 +37,21 @@ def cleanData(censusData):
         # remove second column
         censusData.drop(['Geographic Area Name'], axis=1, inplace = True)
 
-        # delete all columns containing 'margin of error'
+        # remove last column (a rogue NaN)  ***NEED TO WRITE A TEST FOR THIS, NOT SURE WHY IT SHOWS UP***
+        censusData = censusData.iloc[:,:-1]
 
-        print(censusData.columns)
+        # delete all columns containing 'margin of error' // there's a rogue np.flat64(nan) column at the end of my df that's forking things up
+        marginsOfError = [col for col in censusData.columns if "Margin of Error" in col]
+        censusData.drop(censusData[marginsOfError], axis=1, inplace = True)
+
+        #print(censusData[marginsOfError])
+        
+        print(list(censusData.columns))
 
         # censusData.to_csv("cleanData.csv") # create new file for data output
 
     except Exception as issue:
-        print("Error occured: ", issue)
+        print("Oops! An error occured: ", issue)
 
 
 
